b4f3085864fce275f0ebc13925214056c031040a
Tuan Nguyen <Nguyenvantuan.itus@gmail.com>
2016-12-17 00:21:06 +0700

    Fix conflict

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -4,7 +4,7 @@
 
 let mongoose = require('mongoose'),
     chalk = require('chalk'),
-    errorCtrl = require('./response.controller.js');
+    errorCtrl = require('./error.controller');
 
 
 let Category = mongoose.model('Category');
